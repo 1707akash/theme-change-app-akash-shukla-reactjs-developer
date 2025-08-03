5d098ca57f830d763f765134dafe14c7246a0561
akashshukla-gloify <akash.shukla@gloify.com>
2025-08-04 01:34:30 +0530

    unused imports removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import React,{ StrictMode } from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
